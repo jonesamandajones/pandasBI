cbbccab7d5ea151a4f54462d16e4eb856e29ff33
amanda jones <amandaoliveirajones@gmail.com>
2023-02-20 11:27:50 -0300

    adicionando coluna

--- main.py
+++ main.py
@@ -8,4 +8,8 @@ df['ID'] = pd.to_numeric(df['ID'])
 
 df['DATA_NASCIMENTO'] = pd.to_datetime(df['DATA_NASCIMENTO'])
 
+df['SEXO'] = df['SEXO'].replace(['F'], 'FEMININO')
+
+df['SEXO'] = df['SEXO'].replace(['M'], 'MASCULINO')
+
 print(df)
